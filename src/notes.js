9fb7224296b05076246fd0a99ff19b8d0fda567d
Aanya Shukla <aanyashukla67@gmail.com>
2024-11-10 19:51:41 +0530

    Initial commit

---
+++ src/notes.js
@@ -0,0 +1,40 @@
+const notes = [
+  {
+    key: 1,
+    title: "Delegation",
+    content:
+      "Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem",
+  },
+  {
+    key: 2,
+    title: "Loops",
+    content:
+      "How to keep a programmer in the shower forever. Show him the shampoo bottle instructions: Lather. Rinse. Repeat.",
+  },
+  {
+    key: 3,
+    title: "Arrays",
+    content:
+      "Q. Why did the programmer quit his job? A. Because he didn't get arrays.",
+  },
+  {
+    key: 4,
+    title: "Hardware vs. Software",
+    content:
+      "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software.",
+  },
+  {
+    key: 5,
+    title: "CSS",
+    content:
+      "CSS is like a magical wardrobe that turns your HTML from “meh” to “wow!” — you pick the outfits (colors, fonts), and make sure no element shows up looking like it rolled out of bed.",
+  },
+  {
+    key: 6,
+    title: "Dijkstra’s Algorithm",
+    content:
+      'Dijkstra’s algorithm is like a GPS that’s super efficient but also really grumpy—"No, we’re not stopping for snacks, just get to the shortest path!"',
+  },
+];
+
+export default notes;
